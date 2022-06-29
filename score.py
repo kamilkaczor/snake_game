d1ea52d86799a42c829cd6f4ec6f4aeabba2d52b
kamilkaczor <kacozr.kamil@gmail.com>
2022-06-29 14:36:05 +0200

    snake

--- score.py
+++ score.py
@@ -1,4 +1,5 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
+
 
 class Score(Turtle):
 
@@ -11,6 +12,7 @@ class Score(Turtle):
         self.penup()
         self.goto(0, 260)
         self.write(f"Score = {self.score}", align="center", font=('Arial', 24, 'normal') )
+        self.screen = Screen()
 
     def add_point(self):
         self.score += 1
@@ -24,10 +26,15 @@ class Score(Turtle):
 
     def get_highscore(self):
         with open("highscore.txt", mode="r") as file:
-            self.high_score = int(file.read())
+            high_score = file.read()
+            self.high_score = int(high_score)
+
 
     def save_highscore(self):
         with open("highscore.txt", mode="w") as file:
             if self.score > self.high_score:
                 self.high_score = self.score
-                file.write(str(self.high_score))
+                file.write( str(self.high_score))
+
+
+#{"player": "player1", "score":0}
